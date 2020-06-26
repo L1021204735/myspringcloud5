f151a3627cbe991f2497b18007ddbce3a946ea7a
刘俊伯 <1021204735@qq.com>
2020-06-26 11:48:31 +0800

    commit the myspringcloud2 demo

--- myspringcloud-consumer/src/main/java/com/en/controller/ConsumerProductController.java
+++ myspringcloud-consumer/src/main/java/com/en/controller/ConsumerProductController.java
@@ -1,36 +1,48 @@
 package com.en.controller;
 
 import com.en.po.ProductPo;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 import java.util.List;
-
+/**
+ * @ClassName ConsumerProductController
+ * @Description 服务消费方控制器
+ * @Author liuxiaobai
+ * @Date 2020年6月26日09:18:41
+ * @Version 1.0
+ **/
 @RestController
 @RequestMapping("/consumer")
 public class ConsumerProductController {
-    public static final String PRODUCT_GET_URL = "http://localhost:8081/prodcut/getInfoById/";
-    public static final String PRODUCT_LIST_URL="http://localhost:8081/prodcut/getAllInfo/";
-    public static final String PRODUCT_ADD_URL = "http://localhost:8081/prodcut/addInfo/";
+
+    private static final String PRODUCT_GET_URL = "http://localhost:8081/prodcut/getInfoById/";
+    private static final String PRODUCT_LIST_URL="http://localhost:8081/prodcut/getAllInfo/";
+    private static final String PRODUCT_ADD_URL = "http://localhost:8081/prodcut/addInfo/";
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private HttpHeaders httpHeaders;
 
     @RequestMapping("/product/getInfoById")
     public Object getProduct(long id) {
 
-        return restTemplate.getForObject(PRODUCT_GET_URL + id, ProductPo.class);
+        return  restTemplate.exchange(PRODUCT_GET_URL + id,HttpMethod.GET,new HttpEntity<>(httpHeaders), ProductPo.class).getBody();
     }
 
     @RequestMapping("/product/getAllInfo")
     public  Object listProduct() {
 
-        return restTemplate.getForObject(PRODUCT_LIST_URL, List.class);
+        return restTemplate.exchange(PRODUCT_LIST_URL,HttpMethod.GET,new HttpEntity<>(httpHeaders), List.class).getBody();
     }
 
     @RequestMapping("/product/addInfo")
     public Object addPorduct(ProductPo productPo) {
 
-        return restTemplate.postForObject(PRODUCT_ADD_URL, productPo, Boolean.class);
+        return  restTemplate.exchange(PRODUCT_ADD_URL, HttpMethod.POST,new HttpEntity<>(productPo,httpHeaders), Boolean.class).getBody();
     }
 }
--- myspringcloud-provider-product/src/main/java/com/en/ProductApplication.java
+++ myspringcloud-provider-product/src/main/java/com/en/ProductApplication.java
@@ -3,6 +3,7 @@ package com.en;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @ClassName ProductApplication
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  **/
 @SpringBootApplication
 @MapperScan("com.en.mapper")
+@EnableEurekaClient
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class,args);
