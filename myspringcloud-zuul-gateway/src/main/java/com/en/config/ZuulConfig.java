b71f1bcf53a424609c9bf644610e1ecd5774cecb
刘俊伯 <1021204735@qq.com>
2020-07-07 21:30:57 +0800

    commit the myspringcloud5 demo

---
+++ myspringcloud-zuul-gateway/src/main/java/com/en/config/ZuulConfig.java
@@ -0,0 +1,13 @@
+package com.en.config;
+
+import com.en.filter.AuthorizedRequestFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ZuulConfig {
+    @Bean
+    public AuthorizedRequestFilter getAuthorizedRequestFilter() {
+        return new AuthorizedRequestFilter() ;
+    }
+}
